fc3953e4118098f61d09bf72305168091965545f
Mayukh Das <mayukh4321@gmail.com>
2026-04-15 23:29:00 +0800

    Revert "revert some template code"
    This reverts commit 2ea45a43425d5b48e6ee5d522e9136ab3772ab4b.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,27 @@
+// using iterators to solve. @it references an interator, and prints it accordingly in the print
+// statement.
+
 pub fn solve() {
     cp::prepare!();
-    sc!(a: isize, b: isize);
-
-    pp!(a + b);
+    sc!(n, mut a: [usize; n]);
+    a.sort_unstable();
+    let mut ans = vec![];
+    let check = |a: &[usize]| {
+        let n = a.len();
+        n % 2 == 0 && (0..n).all(|i| a[i] + a[n - i - 1] == a[0] + a[n - 1])
+    };
+    if check(&a) {
+        ans.push(a[0] + a[n - 1]);
+    }
+    let l = a[n - 1];
+    while a.last() == Some(&l) {
+        a.pop();
+    }
+    if check(&a) {
+        ans.push(l);
+    }
+    ans.sort_unstable();
+    pp!(@it ans);
 }
 
 cp::main!();
